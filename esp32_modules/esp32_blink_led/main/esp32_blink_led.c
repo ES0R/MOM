76aa60de5885fed9afd71a25cf500cdcb2604d94
ES0R <58563157+ES0R@users.noreply.github.com>
2023-07-13 18:40:04 +0200

    blink led

--- esp32_modules/esp32_blink_led/main/esp32_blink_led.c
+++ esp32_modules/esp32_blink_led/main/esp32_blink_led.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
-
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
 
-#include "esp_log.h"
+#define LED_PIN 48 // GPIO 33 controls the on-board LED on ESP32-S3-DevKitC
 
 void app_main(void)
 {
-    char *ourTaskname = pcTaskGetName(NULL);
-    ESP_LOGI(ourTaskname, "Hello, starting up!\n");
+    // Configure UART
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    uart_param_config(UART_NUM_0, &uart_config);
+    uart_set_pin(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_driver_install(UART_NUM_0, 256, 0, 0, NULL, 0);
 
-    while(1){
-        ;;
+    esp_rom_gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        gpio_set_level(LED_PIN, 1); // Turn on the LED
+        vTaskDelay(pdMS_TO_TICKS(500)); // Wait for 500 milliseconds
+
+        gpio_set_level(LED_PIN, 0); // Turn off the LED
+        vTaskDelay(pdMS_TO_TICKS(500)); // Wait for 500 milliseconds
+
     }
 }
